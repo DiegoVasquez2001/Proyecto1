325f443807797926bf144d0f1c4a73506da62d6e
Diego Vásquez <60155800+EngineerDV@users.noreply.github.com>
2020-02-13 10:03:20 -0600

    ACTUALIZACION 130220

--- Diego/Programa/calcularNomina.cpp
+++ Diego/Programa/calcularNomina.cpp
@@ -6,18 +6,19 @@
 using namespace std;
 
 void BuscarEmpleado();
-void CalcularNomina(float pagoHora);
+void CalcularNomina(float pagoHora, string puesto);
 
 int main(){
 	BuscarEmpleado();
-	return 0;
 	system("PAUSE");
+	return 0;
 }
 int id;
 char nombre[60];
 char puesto[100];
 float phora;
 
+int varContador=1;
 
 void BuscarEmpleado(){
 	ifstream lecturaArchivo;
@@ -33,23 +34,37 @@ void BuscarEmpleado(){
 			cout<<"NOMBRE: "<<nombre<<endl;
 			cout<<"PUESTO: "<<puesto<<endl;
 			cout<<"SUELDO HORA: "<<phora<<endl;
-			CalcularNomina(phora);
+			CalcularNomina(phora, puesto);
 			lecturaArchivo>>id;
+			varContador++;
 		}
 	}
 	else{
 		cout<<"ERROR EN CONSULTA"<<endl;
 	}
-
 	lecturaArchivo.close();
 }
 
-float r;
 float ingresosFijos;
+float ingresosVariables;
+const int bonIncentivo=250;
+float sueldoExtra;
+float descuentosFijos;
+float igss;
+float isr;
 
-void CalcularNomina(float pagoHora){
-	cout<<"COMPUTE INGRESOS FIJOS: ";
-	cin>>ingresosFijos;
-	r=((pagoHora*8)*30)+ingresosFijos;
-	cout<<r<<"\n";
+void CalcularNomina(float pagoHora, string puesto){
+	cout<<"COMPUTE INGRESOS VARIABLES: ";
+	cin>>ingresosVariables;
+
+	ingresosFijos=((pagoHora*8)*30)+ingresosVariables+bonIncentivo;
+	cout<<ingresosFijos<<"\n";
+
+	if(puesto=="Administrador"){
+		cout<<"EXONERADO DE IGSS!"<<"\n";
+	}
+	else{
+		igss = ingresosFijos*0.0483;
+		cout<<igss<<"\n";
+	}
 }
